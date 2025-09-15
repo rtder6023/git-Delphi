88d4a07261f4110e6efb3497cfd1affcaaea693a
rtder6023@gmail.com <rtder6023>
2025-09-16 08:46:54 +0900

    TEST

---
+++ Unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Buttons,
+  Vcl.Imaging.jpeg;
+
+type
+  TForm1 = class(TForm)
+    Panel2: TPanel;
+    Panel1: TPanel;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
+    SpeedButton3: TSpeedButton;
+    Panel9: TPanel;
+    SpeedButton4: TSpeedButton;
+    Image1: TImage;
+    Memo1: TMemo;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Image6: TImage;
+    Image7: TImage;
+    Image8: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
